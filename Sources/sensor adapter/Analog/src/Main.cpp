67aff64c8eb9b931ced832a2235b82b368168b99
hadrian javas <hadrianjavas12@gmail.com>
2023-09-27 19:34:07 +0700

    feat(adaptor-analog): Add sent sensor information and and comment
    - include SofwareSerial.h libraries
    - Add sent sensor information via digital pin using SofwareSerial libraries;
    - Add code comment;
    
    Closes

--- Sources/sensor adapter/Analog/src/Main.cpp
+++ Sources/sensor adapter/Analog/src/Main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 
 //ADAPTER INFORMATION
-String sensorName = "Intesitas Cahaya";
-String unit = "lux";
-double m = -2.94;
-double C = 2400.2;
-#define sensorPin A7
-
+String sensorName = "Intesitas Cahaya";  //nama sensor
+String unit = "lux";    //satuan pengukuran sensor
+double m = -2.94;   //kemiringan pada perasamaan linear  Y= m*x + C
+double C = 2400.2;  //koefisien pada perasamaan linear  Y= m*x + C
+#define sensorPin A7  // pin analog yang digunakan
+long int lastTimeSent=0;
+int deliveryIntervalTime = 100; // interval waktu mengirimkan data dalam milidetik
 
  // kalman filter
 float err_measure = 10;
@@ -17,12 +18,23 @@ float q = 0.01;
 
 Sensor analogSensor(sensorName,unit, m, C, err_measure, err_estimate, q);
 
+
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(11, 12); // RX, TX
+
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+ 
   Serial.println(F("Initialization"));
   //inisialisasi program
-  
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  analogSensor.SetRawValue(analogRead(sensorPin));
+
   Serial.println(F("______________________________________________"));
   Serial.println(F("Sensor Parameter"));
   Serial.println(analogSensor.GetSensorFullParam());
@@ -30,6 +42,14 @@ void setup() {
 }
 
 void loop() {
+  //memperbaharui nilai sensor
   analogSensor.UpdateRawValue(analogRead(sensorPin));
-  Serial.println(analogSensor.GetSensorInfo());
+  
+  //mengirimkan nilai sensor information setiap interval waktu
+  if(millis()-lastTimeSent>deliveryIntervalTime){
+    String _sensorInfo = analogSensor.GetSensorInfo();
+    Serial.println(_sensorInfo);
+    mySerial.println(_sensorInfo);
+    lastTimeSent = millis();
+  }  
 }
